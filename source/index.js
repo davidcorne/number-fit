f7f57028ed494d18849c95a4e87773a81d844354
David Corne <davidcorne@gmail.com>
2023-03-29 14:12:03 +0000

    Display the grid properly in index.

--- source/index.js
+++ source/index.js
@@ -2,7 +2,7 @@ const gridModule = require("./grid")
 
 function main() {
     const g = gridModule.Grid.fromString("4,3,123##4##5678")
-    console.log(g)
+    console.log(g.toString())
 }
 
 main()
