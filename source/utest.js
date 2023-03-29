e7735d0fc388a3aa6ef9fc0c811f47a87a10e6b9
David Corne <davidcorne@gmail.com>
2023-03-29 07:33:19 +0000

    Init project and start testing

---
+++ source/utest.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const rewire = require("rewire")
+
+const gridModule = rewire("./grid.js")
+
+describe("Grid", function() {
+    it("Construct from string", function() {
+        
+    })
+})
